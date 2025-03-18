4c7967b9748b4fd7c42ef9fb97efe1fcd83b6aad
Rubens Borges <contact@rubensborges.dev>
2025-03-18 17:28:43 -0300

    feat: added avaliacoes router

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const sequelize = require('./database');  // Conexão com o banco de dados
-const ofertasRoutes = require('./routes/ofertasRoutes');
-const usuariosRoutes = require('./routes/usuariosRoutes');
-const milhasRoutes = require('./routes/milhasRoutes');
-const negociacaoRoutes = require('./routes/negociacaoRoutes');
-const loginRoutes = require('./routes/loginRoutes');
-const garantiaRoutes = require('./routes/garantiaRoutes');
-const reservaRoutes = require('./routes/paymentRoutes');
-  
+const sequelize = require("./database"); // Conexão com o banco de dados
+const ofertasRoutes = require("./routes/ofertasRoutes");
+const usuariosRoutes = require("./routes/usuariosRoutes");
+const milhasRoutes = require("./routes/milhasRoutes");
+const negociacaoRoutes = require("./routes/negociacaoRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+const garantiaRoutes = require("./routes/garantiaRoutes");
+const reservaRoutes = require("./routes/paymentRoutes");
+const avaliationRouter = require("./routes/avaliacoes");
 
 const app = express();
-const port = 5000;
+const port = 5005;
 
 // Middleware
 app.use(cors());
@@ -21,31 +21,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Teste de Conexão com o Banco de Dados
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Conexão com o banco de dados bem-sucedida!');
+    console.log("Conexão com o banco de dados bem-sucedida!");
   })
   .catch((error) => {
-    console.error('Erro ao conectar com o banco de dados:', error);
+    console.error("Erro ao conectar com o banco de dados:", error);
   });
 
 // Sincronização do Banco de Dados
-sequelize.sync({ alter: true })
+sequelize
+  .sync({ alter: true })
   .then(() => {
-    console.log('Banco de dados sincronizado');
+    console.log("Banco de dados sincronizado");
     app.listen(port, () => {
       console.log(`Servidor rodando na porta ${port}`);
     });
   })
   .catch((error) => {
-    console.error('Erro ao sincronizar banco de dados:', error);
+    console.error("Erro ao sincronizar banco de dados:", error);
   });
 
 // Usar as rotas
-app.use('/api/ofertas', ofertasRoutes);
-app.use('/api/usuarios', usuariosRoutes);
-app.use('/api', milhasRoutes);
-app.use('/api/negociacao', negociacaoRoutes);
-app.use('/api', loginRoutes);
-app.use('/api/garantias', garantiaRoutes); 
-app.use('/api', reservaRoutes);
+app.use("/api/ofertas", ofertasRoutes);
+app.use("/api/usuarios", usuariosRoutes);
+app.use("/api", milhasRoutes);
+app.use("/api/negociacao", negociacaoRoutes);
+app.use("/api", loginRoutes);
+app.use("/api/avaliacoes", avaliationRouter);
+app.use("/api/garantias", garantiaRoutes);
+app.use("/api", reservaRoutes);
