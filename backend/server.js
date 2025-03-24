8eece1d98bafe48131c776c7220720a00bbfdfee
Rubens Borges <contact@rubensborges.dev>
2025-03-24 18:05:34 -0300

    fix: change for 5003 port

--- backend/server.js
+++ backend/server.js
@@ -2,54 +2,48 @@
 const express = require("express");
 const cors = require("cors");
 
-
-const sequelize = require('./database');  // Conexão com o banco de dados
-const ofertasRoutes = require('./routes/ofertasRoutes');
-const usuariosRoutes = require('./routes/usuariosRoutes');
-const milhasRoutes = require('./routes/milhasRoutes');
-const negociacaoRoutes = require('./routes/negociacaoRoutes');
-const loginRoutes = require('./routes/loginRoutes');
-const garantiaRoutes = require('./routes/garantiaRoutes');
-const reservaRoutes = require('./routes/paymentRoutes');
+const sequelize = require("./database"); // Conexão com o banco de dados
+const ofertasRoutes = require("./routes/ofertasRoutes");
+const usuariosRoutes = require("./routes/usuariosRoutes");
+const milhasRoutes = require("./routes/milhasRoutes");
+const negociacaoRoutes = require("./routes/negociacaoRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+const garantiaRoutes = require("./routes/garantiaRoutes");
+const reservaRoutes = require("./routes/paymentRoutes");
 const cleanIndexes = require("./cleanIndexes.js");
 const avaliationRouter = require("./routes/avaliacoes");
 
-
 const app = express();
-const port = 5000;
+const port = 5003;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Executar a limpeza de índices antes de iniciar o servidor
 cleanIndexes()
   .then(() => {
     console.log("🔍 Índices verificados e limpos!");
-    
+
     // Teste de Conexão com o Banco de Dados
     return sequelize.authenticate();
   })
   .then(() => {
-    console.log('✅ Conexão com o banco de dados bem-sucedida!');
-    
+    console.log("✅ Conexão com o banco de dados bem-sucedida!");
+
     // Sincronização do Banco de Dados
     return sequelize.sync({ alter: true });
   })
   .then(() => {
-    console.log('✅ Banco de dados sincronizado');
-    
+    console.log("✅ Banco de dados sincronizado");
 
     app.listen(port, () => {
       console.log(`🚀 Servidor rodando na porta ${port}`);
     });
   })
   .catch((error) => {
-
-    console.error('❌ Erro ao iniciar o servidor:', error);
-
+    console.error("❌ Erro ao iniciar o servidor:", error);
   });
 
 // Usar as rotas
