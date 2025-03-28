f1b512aed731dff4c286c9cc43506618029d7179
Rubens Borges <contact@rubensborges.dev>
2025-03-28 00:24:30 -0300

    feat: created new route

--- backend/routes/negociacaoRoutes.js
+++ backend/routes/negociacaoRoutes.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const negociacaoController = require('../controllers/negociacaoController');
-const authMiddleware = require('../authMiddleware');  // Middleware de autenticação
+const negociacaoController = require("../controllers/negociacaoController");
+const authMiddleware = require("../authMiddleware"); // Middleware de autenticação
 
 // Buscar todas as negociações
-router.get('/', negociacaoController.buscarNegociacoes);
+router.get("/", negociacaoController.buscarNegociacoes);
 
 // Buscar negociações do usuário autenticado (com base no token JWT)
-router.get('/usuario', authMiddleware, negociacaoController.getNegociacoesPorUsuario);  // Altere para não precisar de :usuarioId na URL
+router.get(
+  "/usuario",
+  authMiddleware,
+  negociacaoController.getNegociacoesPorUsuario
+); // Altere para não precisar de :usuarioId na URL
 
 // Buscar detalhes de uma negociação específica
-router.get('/:id', negociacaoController.buscarNegociacaoPorId);
+router.get("/:id", negociacaoController.buscarNegociacaoPorId);
 
-// Rota para criar uma nova negociação  
-router.post('/', negociacaoController.adicionarNegociacao);
+// Rota para criar uma nova negociação
+router.post("/", negociacaoController.adicionarNegociacao);
+router.put("/:ratedId/:ratingID", negociacaoController.atualizarAvaliacoes);
 
-router.put('/:id/status', negociacaoController.atualizarStatusNegociacao);
+router.put("/:id/status", negociacaoController.atualizarStatusNegociacao);
 
 module.exports = router;
