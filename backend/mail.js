2624a040ee195be6c8e1df9dbe908ecd2e0f1f2f
Gabriel Patatt <gabrielpatatt@gmail.com>
2025-04-08 22:05:55 -0300

    ajustado email smpt etc

--- backend/mail.js
+++ backend/mail.js
@@ -1,33 +1,33 @@
 const nodemailer = require("nodemailer");
-const AWS = require("aws-sdk");
 require('dotenv').config(); // para carregar o .env
 
-// Configura a AWS (credenciais devem estar nas variáveis de ambiente)
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "us-east-1", // ou a região que você escolheu no SES
-});
-
-// Cria o transporte com SES
+// Cria o transporte SMTP (Brevo, Mailersend ou outro)
 const transport = nodemailer.createTransport({
-  SES: new AWS.SES({ apiVersion: "2010-12-01" }),
+  host: process.env.SMTP_HOST, // ex: smtp-relay.brevo.com
+  port: 587,
+  secure: false, // true para porta 465
+  auth: {
+    user: process.env.SMTP_USER, // seu e-mail verificado na Brevo
+    pass: process.env.SMTP_PASS, // senha SMTP da Brevo (ou chave da API SMTP)
+  },
 });
 
 // Verifica a conexão
 transport.verify(function (error, success) {
   if (error) {
-    console.log("Erro ao verificar o transporte SES:", error);
+    console.log("Erro ao verificar o transporte SMTP:", error);
   } else {
-    console.log("Transport SES verificado com sucesso!");
+    console.log("Transporte SMTP verificado com sucesso!");
   }
 });
 
-// Função para enviar email
+// Função para enviar e-mail
 const sendEmail = async (email, subject, message) => {
   try {
+    
+console.log('Cheguei no mandar email' + message);
     const info = await transport.sendMail({
-      from: '"Miles Exchange" <no-reply@milhasexchange.com>', // precisa estar verificado no SES
+      from: '"MilhasExchange" <no-reply@milhasexchange.com>',
       to: email,
       subject,
       text: message,
